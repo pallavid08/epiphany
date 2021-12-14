3029e7679d71abf3bee90d765b18d75b5d024afc
pallavid08 <pallavid0808@gmail.com>
2021-12-14 15:07:13 +0100

    added second card with stars

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const catHiss = document.getElementById('cat-card-hiss');
 const catMeow = document.getElementById('cat-card-meow');
 const catPrrr = document.getElementById('cat-card-prrr');
 
+const stars = document.querySelectorAll('.stars');
+const star = document.getElementById('star');
+
 const nextBtn = document.getElementById('new-btn');
 
 //Functions for Event Listeners
@@ -17,17 +20,33 @@ const handleClickMeow = () => {
    alt = 'Dr Who';
 };
 const handleClickPrrr = () => {
-   imgCard.src = './img/img_statement_4.png';
+   imgCard.src = './img/img_statement_2.png';
    alt = 'Snape';
 };
 
-//Adding Event Listeners for Clicks
+const handleImageChange = () => {
+   console.log('clickedddd');
+   // imgCard.src = './img/img_statement_4.png';
+   // for (let i = 1; i <= 6; i++) {
+   //    imgCard.src = `./img/img_statement_${2}.png`;
+   //    alt = 'new quote image';
+   // }
+};
+//Adding Event Listeners for Clicks on Cat Faces
 catHiss.addEventListener('click', handleClickHiss);
 catMeow.addEventListener('click', handleClickMeow);
-catPrr.addEventListener('click', handleClickPrrr);
+catPrrr.addEventListener('click', handleClickPrrr);
+
+star.addEventListener('click', handleImageChange);
 
 const clicked = () => {
    console.log('clickkk');
 };
 
 nextBtn.addEventListener('click', clicked);
+
+stars.forEach((star, id) => {
+   star.addEventListener('click', () => {
+      console.log(`Star of index ${id} was clicked`);
+   });
+});
