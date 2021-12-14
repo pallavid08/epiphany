f5173c23cdc0b2028ab613c42e6b1f45ad8360d2
pallavid08 <pallavid0808@gmail.com>
2021-12-14 19:29:50 +0100

    Some cleaning and renaming

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const catHiss = document.getElementById('cat-card-hiss');
 const catMeow = document.getElementById('cat-card-meow');
 const catPrrr = document.getElementById('cat-card-prrr');
 
-//Creating variables
-const imgCardSecond = document.getElementById('card-image-second');
-
 //Functions for Event Listeners
 const handleClickHiss = () => {
    imgCard.src = './img/img_statement_4.png';
@@ -22,15 +19,6 @@ const handleClickPrrr = () => {
    imgCard.alt = 'Snape';
 };
 
-const handleImageChange = () => {
-   console.log('clickedddd');
-   imgCard.src = './img/img_statement_4.png';
-   alt = 'image';
-   // for (let i = 1; i <= 6; i++) {
-   //    imgCard.src = `./img/img_statement_${2}.png`;
-   //    alt = 'new quote image';
-   // }
-};
 //Adding Event Listeners for Clicks on Cat Faces
 catHiss.addEventListener('click', handleClickHiss);
 catMeow.addEventListener('click', handleClickMeow);
--- script2.js
+++ script2.js
@@ -1,15 +1,17 @@
 //Creating variables to click on stars
 const imgCardSecond = document.getElementById('card-image-second');
 const star = document.getElementById('stars');
+const starDiv = document.getElementById('stars-text-container');
 
 //Adding Event Listener for Star Clicks
 let imgCount = 2;
 star.addEventListener('click', function () {
    imgCardSecond.src = `./img/img_statement_${imgCount}.png`;
-   //    imgCardSecond.alt = 'Quote Image';
 
    if (imgCount > 6) {
-      window.location = '/third.html';
+      imgCardSecond.src = './img/img_control_tool_finish.svg';
+      imgCardSecond.style.width = '50px';
+      starDiv.parentElement.removeChild(starDiv);
    }
    imgCount++;
 });
