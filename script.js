1241e89c811b17888ce9132208a61f5c3f97eebf
pallavid08 <pallavid0808@gmail.com>
2021-12-14 13:49:41 +0100

    Added javascript for image change on click

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const newBtn = document.getElementById('new-btn');
+//Creating variables to click on cat images
+const imgCard = document.getElementById('card-image');
+const catHiss = document.getElementById('cat-card-hiss');
+const catMeow = document.getElementById('cat-card-meow');
+const catPrrr = document.getElementById('cat-card-prrr');
+
+const nextBtn = document.getElementById('new-btn');
+
+//Functions for Event Listeners
+const handleClickHiss = () => {
+   imgCard.src = './img/img_statement_4.png';
+   alt = 'Master Splinter';
+};
+
+const handleClickMeow = () => {
+   imgCard.src = './img/img_statement_3.png';
+   alt = 'Dr Who';
+};
+const handleClickPrrr = () => {
+   imgCard.src = './img/img_statement_4.png';
+   alt = 'Snape';
+};
+
+//Adding Event Listeners for Clicks
+catHiss.addEventListener('click', handleClickHiss);
+catMeow.addEventListener('click', handleClickMeow);
+catPrr.addEventListener('click', handleClickPrrr);
 
 const clicked = () => {
    console.log('clickkk');
-   const h2 = document.createElement('h2');
-   h2.textContent = 'Helloooooo';
-   document.body.appendChild(h2);
 };
 
-newBtn.addEventListener('click', clicked);
+nextBtn.addEventListener('click', clicked);
