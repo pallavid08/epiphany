ac19ca21907130c1a49611f88b1291ac58246460
pallavid08 <pallavid0808@gmail.com>
2021-12-14 17:45:33 +0100

    Card with both options

--- script2.js
+++ script2.js
@@ -1,8 +1,14 @@
+//Creating variables to click on stars
 const imgCardSecond = document.getElementById('card-image-second');
 const star = document.getElementById('stars');
 
+let imgCount = 2;
 star.addEventListener('click', function () {
-   let i = 4;
-   imgCardSecond.src = `./img/img_statement_${i}.png`;
-   //   imgCardSecond.alt = 'New Image';
+   imgCardSecond.src = `./img/img_statement_${imgCount}.png`;
+   imgCardSecond.alt = 'Quote Image';
+
+   if (x > 6) {
+      window.location = '/third.html';
+   }
+   x++;
 });
